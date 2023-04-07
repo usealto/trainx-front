81995a23b08d3c6697d7a98c35694d546fe37f01
Guillim <guigloo@msn.com>
2023-04-07 16:07:01 +0200

    first ugly steps

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,27 @@
+import { AdminCompaniesComponent } from './components/admin-companies/admin-companies.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminHomeComponent,
+    // adding a layout to this route called AdminLayoutComponent
+    // this layout will be used for all the routes under this path
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: AdminHomeComponent,
+      },
+      {
+        path: 'companies',
+        component: AdminCompaniesComponent,
+      }
+    ]
+
+
   },
 ];
 
