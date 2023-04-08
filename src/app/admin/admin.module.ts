1b60bd5eb2e76e8e64e2d76b818db3a4f5feffb5
Guillim <guigloo@msn.com>
2023-04-08 12:33:02 +0200

    [scss] split files to separate CSS properly

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,9 +8,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
 import { AdminCompaniesComponent } from './components/admin-companies/admin-companies.component';
 import { AdminCompanyComponent } from './components/admin-company/admin-company.component';
+import { AdminMenuComponent } from './components/admin-layout/admin-menu/admin-menu.component';
 
 @NgModule({
-  declarations: [AdminHomeComponent, AdminLayoutComponent, AdminCompaniesComponent, AdminCompanyComponent],
+  declarations: [AdminHomeComponent, AdminLayoutComponent, AdminCompaniesComponent, AdminCompanyComponent, AdminMenuComponent],
   imports: [CommonModule, AdminRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class AdminModule {}
--- src/app/admin/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/components/admin-layout/admin-layout.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'alto-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss','../../../layout/menu/menu.component.scss','../../../layout/app-layout/app-layout.component.scss'],
+  styleUrls: ['./admin-layout.component.scss','../../../layout/app-layout/app-layout.component.scss'],
 })
 export class AdminLayoutComponent {}
