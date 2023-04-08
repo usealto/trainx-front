07faeda595e2e2538369b57900c7cf8338689b1d
Guillim <guigloo@msn.com>
2023-04-08 11:41:10 +0200

    [scss] refacto to inherit more than to copy

--- src/app/admin/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/components/admin-layout/admin-layout.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'alto-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss','../../../layout/menu/menu.component.scss'],
+  styleUrls: ['./admin-layout.component.scss','../../../layout/menu/menu.component.scss','../../../layout/app-layout/app-layout.component.scss'],
 })
 export class AdminLayoutComponent {}
