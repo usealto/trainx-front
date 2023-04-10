82715541391fb50515d6db009350429b62aece8b
Guillim <guigloo@msn.com>
2023-04-10 20:02:11 +0200

    [user creation] preparing the creation we need the rest service to be created on main branch

---
+++ src/app/admin/components/admin-user-create/admin-user-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alto-admin-user-create',
+  templateUrl: './admin-user-create.component.html',
+  styleUrls: ['./admin-user-create.component.scss']
+})
+export class AdminUserCreateComponent {
+
+}
