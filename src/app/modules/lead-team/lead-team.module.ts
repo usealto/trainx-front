22db38803c83cbe67b532bec2fdfc5bdda18f5c6
hugo poisot <hugo.poisot@epitech.eu>
2023-04-12 13:53:17 +0200

    [Navigation]Add anchors navigation on teams and programs page

--- src/app/modules/lead-team/lead-team.module.ts
+++ src/app/modules/lead-team/lead-team.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LeadTeamRoutingModule } from './lead-team-routing.module';
 import { LeadTeamComponent } from './components/lead-team/lead-team.component';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
-  declarations: [LeadTeamComponent],
-  imports: [CommonModule, LeadTeamRoutingModule],
+    declarations: [LeadTeamComponent],
+    imports: [CommonModule, LeadTeamRoutingModule, SharedModule]
 })
 export class LeadTeamModule {}
