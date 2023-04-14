4842bd107fbacea292a311222d459fccd8ca6316
Guillim <guigloo@msn.com>
2023-04-14 17:16:13 +0200

    [company] update some fields also hide JSON  by default

--- src/app/admin/components/admin-company/admin-company.component.ts
+++ src/app/admin/components/admin-company/admin-company.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs';
 import { CompaniesRestService } from 'src/app/modules/companies/service/companies-rest.service';
 import { CompanyApi, UserApi } from 'src/app/sdk';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'alto-admin-company',
@@ -15,8 +16,10 @@ export class AdminCompanyComponent implements OnInit {
   company!: CompanyApi;
   users: UserApi[] = [];
   id: string | undefined;
+  companyForm: any;
+  display = false;
 
-  constructor(private readonly companiesRestService: CompaniesRestService, private readonly usersRestService:UsersRestService, private route: ActivatedRoute) {}
+  constructor(private readonly companiesRestService: CompaniesRestService, private readonly usersRestService:UsersRestService, private route: ActivatedRoute, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '';
@@ -24,6 +27,12 @@ export class AdminCompanyComponent implements OnInit {
     this.companiesRestService.getCompanyById(this.id)
     .pipe(
       tap((company) => this.company = company)
+    ).pipe(
+      tap((company) => { this.companyForm = this.formBuilder.group({
+        domain: [this.company.domain],
+        name: [this.company.name],
+        isSlackActive: [this.company.isSlackActive],
+      }); })
     )
     .subscribe();
     
@@ -32,5 +41,16 @@ export class AdminCompanyComponent implements OnInit {
       tap((users) => this.users = users)
     )
     .subscribe();
+  }
+
+  async submit() {
+    // update the user with the userFrom value using the userRestService
+    this.companiesRestService.patchCompany(this.company.id, this.companyForm.value).subscribe();
+    
+    // refresh after the API has time to implement the changes
+    // first sleep 1 second
+    await new Promise(f => setTimeout(f, 1000));
+    this.ngOnInit();
+    
   }
 }
