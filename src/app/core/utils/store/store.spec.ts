ca8c05a1d7c33eead4cecd255f86fc257dba41ac
Thomas M <thomas@usealto.com>
2023-04-19 17:15:06 +0200

    Store Expire Time

---
+++ src/app/core/utils/store/store.spec.ts
@@ -0,0 +1,133 @@
+import { tap } from 'rxjs';
+import { Store } from './store';
+import { TestScheduler } from 'rxjs/testing';
+
+class User {
+  name?: string;
+}
+
+let testScheduler: TestScheduler;
+
+describe('StoreClass Basics', () => {
+  const userName = 'test name';
+  const userNewName = 'new name';
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('gets the data', () => {
+    const objTest = new Store<User>({ name: userName });
+
+    expect(objTest.value.name).toBe(userName);
+  });
+
+  it('sets the data', () => {
+    const objTest = new Store<User>({ name: userName });
+
+    objTest.value = { name: userNewName };
+    expect(objTest.value.name).toBe(userNewName);
+  });
+
+  it('checks the Observable emits', () => {
+    const objTest = new Store<User>({ name: userName });
+
+    const expectedMarbles = 'ab';
+    const expectedValues = {
+      a: { name: userName },
+      b: { name: userNewName },
+    };
+    const triggerMarbles = '-a';
+    const triggerValues = {
+      a: () => (objTest.value = { name: userNewName }),
+    };
+    testScheduler.run(({ expectObservable, cold }) => {
+      expectObservable(objTest.value$).toBe(expectedMarbles, expectedValues);
+      expectObservable(cold(triggerMarbles, triggerValues).pipe(tap((fn) => fn())));
+    });
+  });
+
+  it('checks with empty Store Object', () => {
+    const startEmpty: Store<User> = new Store<User>({});
+
+    const expectedMarbles = 'abca';
+    const expectedValues = {
+      a: {},
+      b: { name: userName },
+      c: { name: userNewName },
+    };
+    const triggerMarbles = '-abc';
+    const triggerValues = {
+      a: () => (startEmpty.value = { name: userName }),
+      b: () => (startEmpty.value = { name: userNewName }),
+      c: () => (startEmpty.value = {}),
+    };
+    testScheduler.run(({ expectObservable, cold }) => {
+      expectObservable(startEmpty.value$).toBe(expectedMarbles, expectedValues);
+      expectObservable(cold(triggerMarbles, triggerValues).pipe(tap((fn) => fn())));
+    });
+  });
+
+  it('checks with empty Array', () => {
+    const startEmpty: Store<User[]> = new Store<User[]>([]);
+
+    const expectedMarbles = 'abca';
+    const expectedValues = {
+      a: [],
+      b: [{ name: userName }],
+      c: [{ name: userName }, { name: userNewName }],
+    };
+    const triggerMarbles = '-abc';
+    const triggerValues = {
+      a: () => (startEmpty.value = [{ name: userName }]),
+      b: () => startEmpty.add({ name: userNewName }),
+      c: () => (startEmpty.value = []),
+    };
+
+    testScheduler.run(({ expectObservable, cold }) => {
+      expectObservable(startEmpty.value$).toBe(expectedMarbles, expectedValues);
+      expectObservable(cold(triggerMarbles, triggerValues).pipe(tap((fn) => fn())));
+    });
+  });
+});
+
+describe('StoreClass with Expire Time', () => {
+  const userName = 'test name';
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('checks when expired', () => {
+    const objTest = new Store<string>('', -10);
+
+    objTest.value = userName; // This value should not be returned as the store is considered 'expired'
+
+    const expectedMarbles = 'a';
+    const expectedValues = {
+      a: '',
+    };
+
+    testScheduler.run(({ expectObservable }) => {
+      expectObservable(objTest.value$).toBe(expectedMarbles, expectedValues);
+    });
+  });
+
+  it('checks when not expired', () => {
+    const objTest = new Store<string>('', 10);
+    objTest.value = userName;
+
+    const expectedMarbles = 'a';
+    const expectedValues = {
+      a: userName,
+    };
+
+    testScheduler.run(({ expectObservable }) => {
+      expectObservable(objTest.value$).toBe(expectedMarbles, expectedValues);
+    });
+  });
+});
--- src/app/core/utils/store/store.ts
+++ src/app/core/utils/store/store.ts
@@ -1,27 +1,74 @@
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 export class Store<T> {
+  private initValue: T;
   private holder: BehaviorSubject<T>;
+  /**
+   * UNIX timestamp
+   */
+  private createdAt = 0;
+  /**
+   * N minutes after createdAt
+   */
+  private expire = 0;
 
   get value$(): Observable<T> {
+    this.resetIfExpired();
     return this.holder.asObservable();
   }
 
   get value(): T {
+    this.resetIfExpired();
     return this.holder.value;
   }
 
   set value(v: T) {
+    console.log('v', v);
+
+    console.log('date', new Date().valueOf() - this.createdAt);
+
+    this.updateDate();
+    console.log('isExpired', this.isExpired());
+
     this.holder.next(v);
   }
 
   add(v: unknown) {
     if (Array.isArray(this.holder.value)) {
+      this.updateDate();
       this.holder.next([...this.holder.value, v] as unknown as T);
     }
   }
 
-  constructor(val: T) {
-    this.holder = new BehaviorSubject<T>(val);
+  /**
+   *
+   * @param initValue Default Init value: '{} as T' for an object or [] for array
+   * @param expire Lifetime of this cache in minutes. Default is 15. 0 is forever.
+   */
+  constructor(initValue: T, expire = 15) {
+    this.initValue = initValue;
+    this.holder = new BehaviorSubject<T>(initValue);
+    this.expire = expire;
+    this.updateDate();
+  }
+
+  isExpired(): boolean {
+    return new Date().valueOf() - this.createdAt >= this.expire;
+  }
+
+  private resetIfExpired() {
+    console.log('isExpired', this.isExpired());
+
+    if (this.isExpired()) {
+      if (Array.isArray(this.holder.value)) {
+        this.value = [] as T;
+      } else {
+        this.value = this.initValue;
+      }
+    }
+  }
+
+  private updateDate() {
+    this.createdAt = new Date().valueOf();
   }
 }
