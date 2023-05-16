b492794b5db3ecedabe56fa43a8fc5316a73a2c4
Pierre Capon <pierre.capon.dev@gmail.com>
2023-05-16 21:45:38 +0200

    feat(company-user): fix edit user on upload

--- src/app/admin/components/admin-companies-create/edit-user-upload-form/edit-user-upload-form.component.ts
+++ src/app/admin/components/admin-companies-create/edit-user-upload-form/edit-user-upload-form.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { IFormBuilder, IFormGroup } from 'src/app/core/form-types';
+import { UserDtoApiRolesEnumApi } from 'src/app/sdk';
 
 @Component({
   selector: 'alto-edit-user-upload-form',
@@ -11,6 +12,7 @@ import { IFormBuilder, IFormGroup } from 'src/app/core/form-types';
 export class EditUserUploadFormComponent {
   @Input() user: any;
   private fb: IFormBuilder;
+  rolesPossibleValues = Object.keys(UserDtoApiRolesEnumApi);
   userForm!: IFormGroup<any>;
 
   constructor(public activeOffcanvas: NgbActiveOffcanvas, readonly fob: UntypedFormBuilder) {
@@ -18,9 +20,10 @@ export class EditUserUploadFormComponent {
   }
   ngOnInit(): void {
     this.userForm = this.fb.group<any>({
-      name: ['', [Validators.required]],
+      firstname: [this.user.firstname, [Validators.required]],
+      lastname: [this.user.lastname, [Validators.required]],
       email: [this.user.email, [Validators.email, Validators.required]],
-      programs: [],
+      role: [this.user.role],
     });
   }
 }
