c61295c4aa3781130f71c74e0415a4f702e5faf3
Pierre Capon <pierre.capon.dev@gmail.com>
2023-05-18 16:45:25 +0200

    feat(admin): add breadcrumbs

--- src/app/admin/components/admin-shared/admin-header/admin-header.component.ts
+++ src/app/admin/components/admin-shared/admin-header/admin-header.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter, distinctUntilChanged } from 'rxjs';
 import { coerceBooleanProperty } from 'src/app/modules/shared/helpers/coerce-property';
 
+export interface IBreadCrumb {
+  label: string;
+  url: string | any[];
+}
 @Component({
   selector: 'alto-admin-header',
   templateUrl: './admin-header.component.html',
@@ -17,4 +23,9 @@ export class AdminHeaderComponent {
     this._divider = coerceBooleanProperty(value);
   }
   private _divider = false;
+  @Input() public breadcrumbs: IBreadCrumb[] = [];
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {}
 }
