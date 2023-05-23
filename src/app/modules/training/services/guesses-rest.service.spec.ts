e82523aed0f1c961328d32391fb99fe6c108ff89
Thomas M <thomas@usealto.com>
2023-05-23 18:27:15 +0200

    Fix Test

--- src/app/modules/training/services/guesses-rest.service.spec.ts
+++ src/app/modules/training/services/guesses-rest.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GuessesRestService } from './guesses-rest.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GuessesRestService', () => {
   let service: GuessesRestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(GuessesRestService);
   });
 
