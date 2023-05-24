3a6748dc599f904cbfcd5df72f3aa5c723c15488
Pierre Capon <pierre.capon.dev@gmail.com>
2023-05-24 18:36:22 +0200

    feat(admin): add sub menu for company's users

--- src/app/admin/components/admin-layout/admin-menu/admin-menu.component.ts
+++ src/app/admin/components/admin-layout/admin-menu/admin-menu.component.ts
@@ -6,47 +6,43 @@ import { DataService } from 'src/app/admin/admin-data.service';
 @Component({
   selector: 'alto-admin-menu',
   templateUrl: './admin-menu.component.html',
-  styleUrls: ['./admin-menu.component.scss','../../../../layout/menu/menu.component.scss'],
+  styleUrls: ['./admin-menu.component.scss', '../../../../layout/menu/menu.component.scss'],
 })
 export class AdminMenuComponent implements OnInit {
-  constructor(
-    private readonly usersRestService:UsersRestService,
-    private dataService: DataService,
-    ) { }
-  user!: UserDtoApi
-  userEmail = ''
-  impersonatedUser = localStorage.getItem('impersonatedUser') !== '' 
-  && localStorage.getItem('impersonatedUser')
+  constructor(private readonly usersRestService: UsersRestService, private dataService: DataService) {}
+  user!: UserDtoApi;
+  userEmail = '';
+  impersonatedUser =
+    localStorage.getItem('impersonatedUser') !== '' && localStorage.getItem('impersonatedUser');
 
-  ngOnInit() { 
-    this.refreshMe()
+  ngOnInit() {
+    this.refreshMe();
 
-    this.dataService.data.subscribe(response => {
+    this.dataService.data.subscribe((response) => {
       if (response === 'impersonatedUserUpdated') {
-        this.refreshUserImpersonated()
+        this.refreshUserImpersonated();
       }
     });
   }
 
   refreshMe() {
-    this.usersRestService.getMe().subscribe((user)=> {
-      this.user = user
-      this.userEmail = user.email
-    })
+    this.usersRestService.getMe().subscribe((user) => {
+      console.log(user);
+      this.user = user;
+      this.userEmail = user.email;
+    });
   }
 
   removeImpersonation() {
-    localStorage.setItem('impersonatedUser','')
-    this.impersonatedUser = false
+    localStorage.setItem('impersonatedUser', '');
+    this.impersonatedUser = false;
     this.dataService.sendData('impersonatedUserUpdated');
   }
 
   refreshUserImpersonated() {
-    this.refreshMe()
-    this.impersonatedUser = localStorage.getItem('impersonatedUser') !== '' && localStorage.getItem('impersonatedUser')
-    window.location.reload()
+    this.refreshMe();
+    this.impersonatedUser =
+      localStorage.getItem('impersonatedUser') !== '' && localStorage.getItem('impersonatedUser');
+    window.location.reload();
   }
-
-
-
 }
