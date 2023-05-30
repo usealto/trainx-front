ff962ce5ca648397b3c01c7dbecd741e39847a97
Antoine Berson <antoine.berson01@gmail.com>
2023-05-30 11:42:45 +0200

    404 redirection refacto

--- src/app/layout/not-found/not-found.component.ts
+++ src/app/layout/not-found/not-found.component.ts
@@ -14,7 +14,6 @@ export class NotFoundComponent implements OnInit {
   I18ns = I18ns;
 
   isAdmin = false;
-  displayAdmin = false;
 
   leadRoute = ['/', AltoRoutes.lead, AltoRoutes.leadHome];
   userRoute = ['/', AltoRoutes.user, AltoRoutes.userHome];
@@ -22,19 +21,11 @@ export class NotFoundComponent implements OnInit {
   constructor(public readonly userStore: ProfileStore, private readonly router: Router) {}
 
   ngOnInit(): void {
-    const segments = window.location.pathname.split('/');
     const { roles } = this.userStore.user.value;
     if (
       roles.some((r) => r === UserDtoApiRolesEnumApi.AltoAdmin || r === UserDtoApiRolesEnumApi.CompanyAdmin)
     ) {
       this.isAdmin = true;
-    }
-
-    if (!segments[1]) {
-      this.router.navigate(this.isAdmin ? this.leadRoute : this.userRoute);
-      this.displayAdmin = this.isAdmin;
-    } else {
-      this.displayAdmin = !!segments.length && segments[1] === AltoRoutes.lead;
     }
   }
 }
