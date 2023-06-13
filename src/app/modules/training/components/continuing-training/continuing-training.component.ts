20f707d39e55e557096c165f557e051fc2bbe719
Thomas M <thomas@usealto.com>
2023-06-13 18:56:08 +0200

    [User][Program] Continuous Training - WIP 🕺

--- src/app/modules/training/components/continuing-training/continuing-training.component.ts
+++ src/app/modules/training/components/continuing-training/continuing-training.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ScoreTimeframeEnumApi, ScoreTypeEnumApi } from '@usealto/sdk-ts-angular';
+import { GuessDtoApi, ScoreTimeframeEnumApi, ScoreTypeEnumApi } from '@usealto/sdk-ts-angular';
 import { combineLatest, tap } from 'rxjs';
 import { I18ns } from 'src/app/core/utils/i18n/I18n';
 import { ProfileStore } from 'src/app/modules/profile/profile.store';
@@ -7,6 +7,8 @@ import { AltoRoutes } from 'src/app/modules/shared/constants/routes';
 import { ScoreDuration } from 'src/app/modules/shared/models/score.model';
 import { ScoresRestService } from 'src/app/modules/shared/services/scores-rest.service';
 import { ScoresService } from 'src/app/modules/shared/services/scores.service';
+import { GuessesRestService } from '../../services/guesses-rest.service';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'alto-continuing-training',
@@ -16,14 +18,19 @@ import { ScoresService } from 'src/app/modules/shared/services/scores.service';
 export class ContinuingTrainingComponent implements OnInit {
   I18ns = I18ns;
   AltoRoutes = AltoRoutes;
-
+  daysInPeriod = 30;
+  threshold = 0.42;
+  //
   guessesCount = 0;
   avgScore = 0;
   previousAvgScore = 0;
+  regularity = 0;
+  previousRegularity = 0;
 
   constructor(
     private readonly scoresRestService: ScoresRestService,
     private readonly scoresService: ScoresService,
+    private readonly guessRestService: GuessesRestService,
     private readonly profileStore: ProfileStore,
   ) {}
   ngOnInit(): void {
@@ -43,14 +50,34 @@ export class ContinuingTrainingComponent implements OnInit {
         },
         true,
       ),
+      this.guessRestService.getGuesses({ createdBy: this.profileStore.user.value.id }, ScoreDuration.Month),
+      this.guessRestService.getGuesses(
+        { createdBy: this.profileStore.user.value.id },
+        ScoreDuration.Month,
+        true,
+      ),
     ])
       .pipe(
-        tap(([userScore, previousSCore]) => {
+        tap(([userScore, previousSCore, guesses, previousGuesses]) => {
+          this.regularity = this.getParticipationDays(guesses) / (this.daysInPeriod * this.threshold);
+          this.previousRegularity =
+            this.getParticipationDays(previousGuesses) / (this.daysInPeriod * this.threshold);
+
           this.avgScore = this.scoresService.reduceWithoutNull(userScore.scores[0]?.averages) ?? 0;
           this.previousAvgScore =
             this.scoresService.reduceWithoutNull(previousSCore.scores[0]?.averages) ?? 0;
         }),
       )
       .subscribe();
+  }
+
+  private getParticipationDays(guesses: GuessDtoApi[] = []): number {
+    return guesses.reduce((result, guess) => {
+      const compare = format(guess.createdAt, 'MM/dd/yyyy');
+      if (!result.find((x) => format(x.createdAt, 'MM/dd/yyyy') === compare)) {
+        result.push(guess);
+      }
+      return result;
+    }, [] as GuessDtoApi[]).length;
   }
 }
