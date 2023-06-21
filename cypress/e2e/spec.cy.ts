00cd8489f9fc4f4b0943e9498aa2bc8abdf4fc42
Thomas M <thomas@usealto.com>
2023-06-21 09:56:21 +0200

    Init

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,8 @@
+describe('My First Test', () => {
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Login', function () {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('/');
+    /* ==== End Cypress Studio ==== */
+  });
+});
