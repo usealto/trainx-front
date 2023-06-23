592f960612dbe74e10165925bafcc9e1037c440e
Thomas M <thomas@usealto.com>
2023-06-23 17:01:21 +0200

    Staging Env

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiURL: 'https://api-test-develop.usealto.com',
-  auth0Domain: 'dev-bmttww5s.eu.auth0.com',
-  auth0ClientId: 'ThcIBQZrRso5QaZq67kCU5eFYTfZwTSK',
+  apiURL: 'https://api-staging.osc-fr1.scalingo.io',
+  auth0Domain: 'staging-alto.eu.auth0.com',
+  auth0ClientId: 'HHKbREpvuaqfsSPZwZ0q6WDeY9Pyn0oN',
   airtableURL: 'https://airtable.com/shr0l3s2lxxXhvgao?prefill_Requested+by=',
-  audience : 'https://api.usealto.com'
+  audience: 'https://api.usealto.com',
 };
