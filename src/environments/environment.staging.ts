7b38aaf74894b60d42c2c624f15d1e0964d32135
Thomas M <thomas@usealto.com>
2023-06-23 16:58:25 +0200

    Config for Staging

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://api-test-develop.usealto.com',
+  auth0Domain: 'dev-bmttww5s.eu.auth0.com',
+  auth0ClientId: 'ThcIBQZrRso5QaZq67kCU5eFYTfZwTSK',
+  airtableURL: 'https://airtable.com/shr0l3s2lxxXhvgao?prefill_Requested+by=',
+  audience : 'https://api.usealto.com'
+};
