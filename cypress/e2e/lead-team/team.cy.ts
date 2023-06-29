7d0979e9a3611debaafcd6c5921f2721c56ea3a9
Antoine Berson <antoine.berson01@gmail.com>
2023-06-29 14:36:54 +0200

    E2E Lead Team edit team short/longnames & avoid duplicates

--- cypress/e2e/lead-team/team.cy.ts
+++ cypress/e2e/lead-team/team.cy.ts
@@ -1,16 +1,63 @@
 describe('Lead Team', () => {
   beforeEach(() => {
     cy.loginToAuth0(Cypress.env('auth_username-admin'), Cypress.env('auth_password-admin'));
-    cy.visit('/');
+    cy.visit('/', {});
+    cy.get('[ng-reflect-router-link="l/teams"]').click();
   });
 
   /* ==== Test Created with Cypress Studio ==== */
-  it('loads Home', function () {
+  it('loads Team', () => {
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('[ng-reflect-router-link="l/teams"]').click();
-    cy.get('alto-lead-team > :nth-child(1)').click();
-    cy.get('alto-lead-team > :nth-child(1)').should('have.text', 'Équipes');
+    cy.get('[data-cy="leadTeamTitle"]').click();
+    cy.get('[data-cy="leadTeamTitle"]').should('have.text', 'Équipes');
     /* ==== End Cypress Studio ==== */
+  });
+
+  it('opens edit team panel', () => {
+    cy.get('[data-cy="editTeam"]').first().click();
+  });
+
+  it('edits team shortname and check "already existing" error', () => {
+    cy.get('[data-cy="editTeam"]').first().click();
+
+    cy.get('[data-cy="editShortnameInput"]').click().wait(3000).clear().type('CYP');
+    cy.get('[data-cy="btnSave"').click();
+
+    cy.get('[data-cy="closeEditTeamPanel"]').click();
+    cy.get('[data-cy="toastError"]').contains('409');
+  });
+
+  it('edits team longname and check "already existing" error', () => {
+    cy.get('[data-cy="editTeam"]').first().click();
+
+    cy.get('[data-cy="editLongnameInput"]').click().wait(3000).clear().type('Cypress');
+    cy.get('[data-cy="btnSave"').click();
+
+    cy.get('[data-cy="closeEditTeamPanel"]').click();
+    cy.get('[data-cy="toastError"]').contains('409');
+  });
+
+  it('edits team longname and check it worked', () => {
+    cy.get('[data-cy="editTeam"]').first().click();
+
+    cy.get('[data-cy="editLongnameInput"]').click().wait(3000).clear().type('Cypress Edit');
+    cy.get('[data-cy="btnSave"').click();
+
+    cy.get('[data-cy="closeEditTeamPanel"]').click();
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[data-cy="teamLongname"').first().should('have.text', ' Cypress Edit ');
+    /* ==== End Cypress Studio ==== */
+  });
+
+  it('edits team shortname and check it worked', () => {
+    cy.get('[data-cy="editTeam"]').first().click();
+
+    cy.get('[data-cy="editShortnameInput"]').click().wait(3000).clear().type('CYT');
+    cy.get('[data-cy="btnSave"').click();
+
+    cy.get('[data-cy="closeEditTeamPanel"]').click();
+
+    cy.get('[data-cy="teamShortname"').first().should('have.text', 'CYT');
   });
 });
 
