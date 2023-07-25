3b061bd00db6ca49f971949d0a1435e237045a19
Thomas M <thomas@usealto.com>
2023-07-25 15:47:34 +0200

    Shared Components Done

--- src/app/modules/shared/helpers/progression-pill-arrow.pipe.ts
+++ src/app/modules/shared/helpers/progression-pill-arrow.pipe.ts
@@ -1,18 +1,33 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
   name: 'progressionPillArrow',
 })
 export class ProgressionPillArrowPipe implements PipeTransform {
-  transform(score: number | undefined | null): string {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(score: number | undefined | null): string | SafeHtml {
     if (score === null || score === undefined) {
       return '';
     } else if (score > 0) {
-      return 'bi-arrow-up-short alto-green';
+      return this.sanitizer.bypassSecurityTrustHtml(
+        `<svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="arrow-up-right">
+        <path id="Icon" d="M4.08301 10.4168L9.91634 4.5835M9.91634 4.5835H4.08301M9.91634 4.5835V10.4168" stroke="#12B76A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        </svg>`,
+      );
     } else if (score < 0) {
-      return 'bi-arrow-down-short alto-red';
+      return this.sanitizer.bypassSecurityTrustHtml(
+        `<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="arrow-down-right">
+        <path id="Icon" d="M4.58301 4.08333L10.4163 9.91666M10.4163 9.91666V4.08333M10.4163 9.91666H4.58301" stroke="#F04438" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        </svg>`,
+      );
     } else {
-      return 'bi-dash alto-neutral';
+      return `<i *ngIf="arrow === true" class="fs-4 align-middle bi-dash alto-neutral"/>`;
     }
   }
 }
