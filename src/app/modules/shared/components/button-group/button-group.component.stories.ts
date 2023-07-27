bcf31f15e590fc782a83f3163744c0a520e2b846
Guillim <guigloo@msn.com>
2023-07-27 11:09:10 +0200

    first customizing

--- src/app/modules/shared/components/button-group/button-group.component.stories.ts
+++ src/app/modules/shared/components/button-group/button-group.component.stories.ts
@@ -1,18 +1,25 @@
-
 import type { Meta, StoryObj } from '@storybook/angular';
 import { ButtonGroupComponent } from './button-group.component';
 const meta: Meta<ButtonGroupComponent> = {
+  title: 'Shared/ButtonGroup',
   component: ButtonGroupComponent,
+  tags: ['autodocs'],
+  render: (args: ButtonGroupComponent) => ({
+    props: {
+      ...args,
+    }
+  })
 };
 
 export default meta;
 type Story = StoryObj<ButtonGroupComponent>;
 
 export const Primary: Story = {
-  render: () => ({
-    props: {
-      primary: true,
-      label: 'ButtonGroup',
-    },
-  }),
+  args: {
+    value : 'item1',
+    items: [
+      { label: 'Item 1', value: 'item1' },
+      { label: 'Item 2', value: 'item2' },
+    ],
+  },
 };
