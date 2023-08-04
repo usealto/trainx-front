f99f05376c9aec150bfbb204f2bfc22427ef0979
LyzaMessadene <messadenelyza@gmail.com>
2023-08-04 17:17:03 +0200

    ✨ Create ProfileCard storybook component

---
+++ src/app/modules/shared/components/profile-card/profile-card.component.stories.ts
@@ -0,0 +1,46 @@
+import { type Meta, type StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { ProfileCardComponent } from './profile-card.component';
+import { SharedModule } from '../../shared.module';
+
+const meta: Meta<ProfileCardComponent> = {
+  title: 'Shared/ProfileCard',
+  component: ProfileCardComponent,
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [SharedModule],
+    }),
+  ],
+  render: (args: ProfileCardComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<ProfileCardComponent>;
+
+export const ProfileCard: Story = {
+  args: {
+    user: {
+      id: 'id1',
+      email: 'test@usealto.com',
+      firstname: 'firstname',
+      lastname: 'lastname',
+    },
+  },
+};
+
+export const ProfileCardWhiteText: Story = {
+  args: {
+    user: {
+      id: 'id1',
+      email: 'test@usealto.com',
+      firstname: 'firstname',
+      lastname: 'lastname',
+    },
+    isWhite: true,
+  },
+};
