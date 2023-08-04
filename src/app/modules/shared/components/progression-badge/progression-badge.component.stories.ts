3329ae6534e00f499eca39c09e988ea04c7a8be1
LyzaMessadene <messadenelyza@gmail.com>
2023-08-04 17:17:56 +0200

    ✨ Create ProgressionBadge storybook component

---
+++ src/app/modules/shared/components/progression-badge/progression-badge.component.stories.ts
@@ -0,0 +1,36 @@
+import { type Meta, type StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { ProgressionBadgeComponent } from './progression-badge.component';
+import { SharedModule } from '../../shared.module';
+
+const meta: Meta<ProgressionBadgeComponent> = {
+  title: 'Shared/ProgressionBadge',
+  component: ProgressionBadgeComponent,
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [SharedModule],
+    }),
+  ],
+  render: (args: ProgressionBadgeComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<ProgressionBadgeComponent>;
+
+export const ProgressionBadge: Story = {
+  args: {
+    score: 0.8,
+  },
+};
+
+export const ProgressionBadgeWithoutArrow: Story = {
+  args: {
+    score: 0.8,
+    arrow: false,
+  },
+};
