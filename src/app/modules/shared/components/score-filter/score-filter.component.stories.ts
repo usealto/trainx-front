d81157895bd46967a3c5720578e79a6ba59eddd0
LyzaMessadene <messadenelyza@gmail.com>
2023-08-08 19:11:01 +0200

    ✨ Create ScoreFilter storybook component

---
+++ src/app/modules/shared/components/score-filter/score-filter.component.stories.ts
@@ -0,0 +1,33 @@
+import { type Meta, type StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { ScoreFilterComponent } from './score-filter.component';
+import { SharedModule } from '../../shared.module';
+
+const meta: Meta<ScoreFilterComponent> = {
+  title: 'Shared/ScoreFilter',
+  component: ScoreFilterComponent,
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [SharedModule],
+    }),
+  ],
+  render: (args: ScoreFilterComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<ScoreFilterComponent>;
+
+export const ScoreFilter: Story = {};
+
+// TODO: Add story with SelectedItems
+
+export const ScoreFilterDisabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
