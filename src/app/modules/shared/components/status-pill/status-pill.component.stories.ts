a733039b7b62a6e22f60f9d23fb159ae3fb98568
LyzaMessadene <messadenelyza@gmail.com>
2023-08-08 19:12:31 +0200

    ✨ Create StatusPill storybook component

---
+++ src/app/modules/shared/components/status-pill/status-pill.component.stories.ts
@@ -0,0 +1,36 @@
+import { type Meta, type StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
+import { StatusPillComponent } from './status-pill.component';
+import { SharedModule } from '../../shared.module';
+import { ChallengeDtoApiStatusEnumApi } from '@usealto/sdk-ts-angular';
+
+const meta: Meta<StatusPillComponent> = {
+  title: 'Shared/StatusPill',
+  component: StatusPillComponent,
+  tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [SharedModule],
+    }),
+  ],
+  render: (args: StatusPillComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<StatusPillComponent>;
+
+export const StatusPillInProgress: Story = {
+  args: {
+    status: ChallengeDtoApiStatusEnumApi.InProgress
+  },
+};
+
+export const StatusPillEnded: Story = {
+  args: {
+    status: ChallengeDtoApiStatusEnumApi.Ended
+  },
+};
