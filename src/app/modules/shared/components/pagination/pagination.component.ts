76726548c4f1afe8c5b0fd2bc49e53cfd65e244e
Thomas M <thomas@usealto.com>
2023-08-11 16:00:53 +0200

    Improved Pagination

--- src/app/modules/shared/components/pagination/pagination.component.ts
+++ src/app/modules/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { I18ns } from 'src/app/core/utils/i18n/I18n';
 
 @Component({
@@ -6,12 +6,19 @@ import { I18ns } from 'src/app/core/utils/i18n/I18n';
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
   @Input() collectionSize!: number;
   @Input() pageSize!: number;
-  @Input() maxSize!: number;
   @Input() page!: number;
   @Output() pageChange: EventEmitter<number> = new EventEmitter();
+
+  lastPage = 1;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['collectionSize']) {
+      this.lastPage = Math.ceil(this.collectionSize / this.pageSize);
+    }
+  }
 
   I18ns = I18ns;
 
