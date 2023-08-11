d7fc5e21608a4b321f8db983f38184b97ed9333c
LyzaMessadene <messadenelyza@gmail.com>
2023-08-11 17:43:03 +0200

    ✨ Create ProgramCardList storybook component

---
+++ src/app/modules/shared/components/program-card-list/program-card-list.component.stories.ts
@@ -0,0 +1,24 @@
+import { type Meta, type StoryObj } from '@storybook/angular';
+import { ProgramCardListComponent } from './program-card-list.component';
+
+const meta: Meta<ProgramCardListComponent> = {
+  title: 'Shared/ProgramCardList',
+  component: ProgramCardListComponent,
+  tags: ['autodocs'],
+  render: (args: ProgramCardListComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+};
+
+export default meta;
+type Story = StoryObj<ProgramCardListComponent>;
+
+export const ProgramCardListHome: Story = {};
+
+export const ProgramCardListProgram: Story = {
+  args: {
+    place: 'program',
+  },
+};
