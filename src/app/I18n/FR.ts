f01c99b4da47de1e4c20432bb53f830076b0349a
Thomas M <thomas@usealto.com>
2023-09-04 16:03:27 +0200

    [Lead][Teams] Table Format

--- src/app/I18n/FR.ts
+++ src/app/I18n/FR.ts
@@ -511,7 +511,7 @@ export const FR = {
         noUsers: 'Aucun membre',
         users: ['Aucun', '{{}}', '{{}}'],
         averageScore: 'Score moyen (30 derniers jours)',
-        ativity: 'Activité  moyenne (30 derniers jours)',
+        activity: 'Activité  moyenne (30 derniers jours)',
         noScore: 'Aucun score',
         averageScoreTooltip: 'Score moyen sur les 30 derniers jours',
         creationDate: 'Date de création',
