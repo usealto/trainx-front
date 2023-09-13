04310f6a4de46a4105cc2849006e75479c6c633f
LyzaMessadene <messadenelyza@gmail.com>
2023-09-13 12:57:50 +0200

    change baseURL to env variable

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: Cypress.env('baseURL'),
     experimentalStudio: true,
     viewportWidth: 1400,
     viewportHeight: 860,
