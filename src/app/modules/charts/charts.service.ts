0d507d04da8e04472e2677a9af60e5dda9bcbe22
Thomas M <thomas@usealto.com>
2023-09-15 15:30:05 +0200

    Charts Padding

--- src/app/modules/charts/charts.service.ts
+++ src/app/modules/charts/charts.service.ts
@@ -11,30 +11,27 @@ interface ITooltipParams {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ChartsService {
-  // constructor() { }
-
-  altoFormattingMultiline(eChartsOption: EChartsOption) : EChartsOption {
+  altoFormattingMultiline(eChartsOption: EChartsOption): EChartsOption {
     // replace null or 'n' values with 0 for every series begining with null or 'n'
     if (eChartsOption.series && eChartsOption.series && Array.isArray(eChartsOption.series)) {
       eChartsOption.series.forEach((serie) => {
-        if(serie.type === 'line' && serie.data){
-          serie.data[0] = serie.data[0] === "-" ? 0 : (serie.data[0] ?? 0);
+        if (serie.type === 'line' && serie.data) {
+          serie.data[0] = serie.data[0] === '-' ? 0 : serie.data[0] ?? 0;
 
           serie.emphasis = {
             scale: 4,
             itemStyle: {
-              borderWidth : 3
-            }
-          }
+              borderWidth: 3,
+            },
+          };
 
-          serie.connectNulls = true
-          
+          serie.connectNulls = true;
+          // serie.showSymbol = false;
+          // serie.triggerLineEvent = true;
         }
-        
       });
     }
 
@@ -42,7 +39,7 @@ export class ChartsService {
       trigger: 'item',
       padding: 0,
       borderColor: '#EAECF0',
-      formatter : (params) => {
+      formatter: (params) => {
         const valueFormatter = Array.isArray(eChartsOption.series)
           ? eChartsOption.series[(params as ITooltipParams).seriesIndex].tooltip?.valueFormatter
           : eChartsOption.series?.tooltip?.valueFormatter;
@@ -59,33 +56,39 @@ export class ChartsService {
               <svg xmlns="http://www.w3.org/2000/svg" width="10" height="11" viewBox="0 0 10 11" fill="none">
                 <circle cx="5" cy="5.5" r="5" fill="${(params as ITooltipParams).color}"/>
               </svg>
-              <p>${(params as ITooltipParams).seriesName} : <b style="color: ${(params as ITooltipParams).color}">${formattedData}<b></p>
+              <p>${(params as ITooltipParams).seriesName} : <b style="color: ${
+          (params as ITooltipParams).color
+        }">${formattedData}<b></p>
             </div>
           </div>
         `;
-      }
-    }
+      },
+    };
     eChartsOption.legend = {
       bottom: 0,
       icon: 'circle',
       itemWidth: 8,
-    }
+    };
 
-    return eChartsOption
+    eChartsOption.grid = {
+      left: '8%',
+      top: '30',
+      right: '5%',
+    };
+
+    return eChartsOption;
   }
 
-
-
-  altoFormattingBar(eChartsOption: EChartsOption) : EChartsOption {
+  altoFormattingBar(eChartsOption: EChartsOption): EChartsOption {
     // replace null or 'n' values with 0 for every series begining with null or 'n'
     if (eChartsOption.series && eChartsOption.series && Array.isArray(eChartsOption.series)) {
       eChartsOption.series.forEach((serie) => {
-        if(serie.type === 'bar' ){
+        if (serie.type === 'bar') {
           serie.tooltip = {
             valueFormatter: (value) => {
               return (value as number) + ' %';
             },
-          }
+          };
         }
       });
     }
@@ -94,15 +97,13 @@ export class ChartsService {
       trigger: 'item',
       borderWidth: 1,
       borderColor: '#EAECF0',
-    }
+    };
     eChartsOption.legend = {
       bottom: 0,
       icon: 'circle',
       itemWidth: 8,
-    }
+    };
 
-    return eChartsOption
+    return eChartsOption;
   }
 }
-
-
