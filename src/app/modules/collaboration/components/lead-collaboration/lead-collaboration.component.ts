2a24891876af79529b7843dda8475d267a13804e
oliviermlm <olivier@usealto.com>
2023-09-15 12:06:26 +0200

    Collaboration page header

--- src/app/modules/collaboration/components/lead-collaboration/lead-collaboration.component.ts
+++ src/app/modules/collaboration/components/lead-collaboration/lead-collaboration.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 import { EmojiName } from 'src/app/core/utils/emoji/data';
 import { I18ns } from 'src/app/core/utils/i18n/I18n';
 
+enum ETabValue {
+  PENDING = 'pending',
+  ARCHIVED = 'archived',
+  ALL = 'all',
+}
+
+const labels = {
+  [ETabValue.PENDING]: 'En attente',
+  [ETabValue.ARCHIVED]: 'Archivés',
+  [ETabValue.ALL]: 'Tout voir',
+};
+
+interface ITab {
+  label: string;
+  value: ETabValue;
+}
+
 @Component({
   selector: 'alto-lead-collaboration',
   templateUrl: './lead-collaboration.component.html',
@@ -10,4 +27,18 @@ import { I18ns } from 'src/app/core/utils/i18n/I18n';
 export class LeadCollaborationComponent {
   Emoji = EmojiName;
   I18ns = I18ns;
+
+  tabs: ITab[] = [
+    { label: labels[ETabValue.PENDING], value: ETabValue.PENDING },
+    { label: labels[ETabValue.ARCHIVED], value: ETabValue.ARCHIVED },
+    { label: labels[ETabValue.ALL], value: ETabValue.ALL },
+  ];
+
+  selectedTab = this.tabs[0];
+
+  pendingCount = 2;
+
+  handleTabChange(tab: ITab) {
+    this.selectedTab =  tab;
+  }
 }
