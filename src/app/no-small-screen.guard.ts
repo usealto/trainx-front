e7d1654df7422877c53de2648be36a8261f7c851
Antoine Berson <antoine.berson01@gmail.com>
2023-09-19 17:17:29 +0200

    [NoSmallScreen] Removed comment

--- src/app/no-small-screen.guard.ts
+++ src/app/no-small-screen.guard.ts
@@ -1,4 +1,3 @@
-// import { inject } from '@angular/core';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { debounceTime, fromEvent, map, tap } from 'rxjs';
