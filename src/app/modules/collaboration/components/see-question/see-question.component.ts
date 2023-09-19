9c1b4cd6aa3455f6b0d3bc09d87e7ab3155640cd
Hugo Poisot <poisot.hugo@gmail.com>
2023-09-19 16:54:29 +0200

    Design done

--- src/app/modules/collaboration/components/see-question/see-question.component.ts
+++ src/app/modules/collaboration/components/see-question/see-question.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { QuestionSubmittedDtoApi } from '@usealto/sdk-ts-angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommentDtoApi, QuestionSubmittedDtoApi } from '@usealto/sdk-ts-angular';
 import { combineLatest, map, switchMap, tap } from 'rxjs';
 import { EmojiName } from 'src/app/core/utils/emoji/data';
 import { I18ns } from 'src/app/core/utils/i18n/I18n';
-import { ProfileStore } from 'src/app/modules/profile/profile.store';
+import { CommentsRestService } from 'src/app/modules/programs/services/comments-rest.service';
 import { QuestionsSubmittedRestService } from 'src/app/modules/programs/services/questions-submitted-rest.service';
-
+import { AltoRoutes } from 'src/app/modules/shared/constants/routes';
 @Component({
   selector: 'alto-see-question',
   templateUrl: './see-question.component.html',
@@ -15,12 +15,18 @@ import { QuestionsSubmittedRestService } from 'src/app/modules/programs/services
 export class SeeQuestionComponent implements OnInit {
   Emoji = EmojiName;
   I18ns = I18ns;
+
   submittedQuestion!: QuestionSubmittedDtoApi;
+  comments: CommentDtoApi[] = [];
+
   isLoading = true;
+  breadCrumbElem = '';
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly questionsSubmittedTestService: QuestionsSubmittedRestService,
+    private readonly router: Router,
+    private readonly questionsSubmittedRestService: QuestionsSubmittedRestService,
+    private readonly commentsRestService: CommentsRestService,
   ) {}
 
   ngOnInit(): void {
@@ -29,14 +35,28 @@ export class SeeQuestionComponent implements OnInit {
         map((p) => {
           return p['id'];
         }),
-        switchMap((id) => this.questionsSubmittedTestService.getQuestion(id)),
-        tap((r) => {
-          if (r) {
-            console.log(r);
-            console.log(this.route.firstChild);
-            // this.submittedQuestion = r;
-            this.isLoading = false;
-          }
+        switchMap((id) =>
+          combineLatest([
+            this.questionsSubmittedRestService.getQuestion(id),
+            this.commentsRestService.getComments({ questionId: id }),
+          ]),
+        ),
+        tap({
+          next: ([question, comments]) => {
+            if (question) {
+              console.log(this.route.firstChild);
+              console.log(question);
+              console.log(comments);
+              this.comments = comments;
+              this.submittedQuestion = question;
+              this.isLoading = false;
+            } else {
+              this.router.navigate(['/', AltoRoutes.notFound]);
+            }
+          },
+          error: () => {
+            this.router.navigate(['/', AltoRoutes.notFound]);
+          },
         }),
       )
       .subscribe();
