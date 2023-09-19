d54b00087769a3b85660764832723fca8d7f9cd9
Thomas M <thomas@usealto.com>
2023-09-19 17:00:47 +0200

    [Collaboration] Top Contributors

--- src/app/modules/collaboration/collaboration-routing.module.ts
+++ src/app/modules/collaboration/collaboration-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeadCollaborationComponent } from './components/lead-collaboration/lead-collaboration.component';
+import { TopContributorsComponent } from './components/lead-collaboration/top-contributors/top-contributors.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LeadCollaborationComponent,
+  },
+  {
+    path: 'top-contributors',
+    component: TopContributorsComponent,
   },
 ];
 
