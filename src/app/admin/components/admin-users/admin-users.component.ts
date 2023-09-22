dbf3386262f7d219c1f83f614fb056c438c7576a
Guillim <guigloo@msn.com>
2023-09-22 22:24:57 +0200

    [users auth0] simplification before potential deletion if Romain says its useless

--- src/app/admin/components/admin-users/admin-users.component.ts
+++ src/app/admin/components/admin-users/admin-users.component.ts
@@ -1,8 +1,6 @@
 import { AuthApiService } from '@usealto/sdk-ts-angular';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CompaniesRestService } from 'src/app/modules/companies/service/companies-rest.service';
-import { UsersRestService } from 'src/app/modules/profile/services/users-rest.service';
 import { UserDtoApi } from '@usealto/sdk-ts-angular';
 import { AuthUserGet } from './models/authuser.get';
 import { DataService } from '../../admin-data.service';
@@ -15,7 +13,6 @@ import { DataService } from '../../admin-data.service';
 export class AdminUsersComponent implements OnInit {
   users: UserDtoApi[] = [];
   authusers: AuthUserGet[] = [];
-  companyAdmins: AuthUserGet[] = [];
   id: string | undefined;
   usersCount = 0;
   usersPage = 1;
@@ -23,8 +20,6 @@ export class AdminUsersComponent implements OnInit {
   q = '';
 
   constructor(
-    private readonly companiesRestService: CompaniesRestService,
-    private readonly usersRestService: UsersRestService,
     private readonly authApiService: AuthApiService,
     private route: ActivatedRoute,
     private dataService: DataService,
@@ -33,7 +28,6 @@ export class AdminUsersComponent implements OnInit {
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') || '';
     this.changeUsersPage(this.q);
-    this.fetchCompanyAdmins();
   }
 
   changeUsersPage(query: string) {
@@ -44,22 +38,6 @@ export class AdminUsersComponent implements OnInit {
     this.authApiService.getAuth0Users({q:queryFormated}).subscribe((q) => {
       this.authusers = q.data;
     });
-  }
-
-  fetchCompanyAdmins() {
-    this.authApiService.getRoleUsers({role:'company-admin'})
-    .subscribe((q) => {
-      this.companyAdmins = q.data;
-    });
-  }
-
-  hasRoleCompanyAdmin(email: string) {
-    return this.companyAdmins.find((user) => user.email === email);
-  }
-
-  setImpersonation(email: string) {
-    localStorage.setItem('impersonatedUser', email.toLowerCase());
-    this.dataService.sendData('impersonatedUserUpdated');
   }
 
   setLocalStorage(email: string) {
