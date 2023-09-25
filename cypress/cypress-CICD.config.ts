b28bc91652e85a60f6e00fcb2ad217c94581fe43
Thomas M <thomas@usealto.com>
2023-09-25 15:25:59 +0200

    Config Cypress

--- cypress/cypress-CICD.config.ts
+++ cypress/cypress-CICD.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'https://app-develop.usealto.com',
-    experimentalStudio: true,
     viewportWidth: 1400,
     viewportHeight: 860,
     video: false,
