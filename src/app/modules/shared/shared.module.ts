01bce3384efb91745bbb00c96839a795b2bd2c56
Hugo Poisot <poisot.hugo@gmail.com>
2023-09-29 17:39:40 +0200

    Add leaderboard component and replace tooflops

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -46,6 +46,7 @@ import { TrainingCardComponent } from './components/training-card/training-card.
 import { ProgressionPillArrowPipe } from './helpers/progression-pill-arrow.pipe';
 import { ProgressionPillPipe } from './helpers/progression-pill.pipe';
 import { TeamColorPipe } from './helpers/team-color.pipe';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
@@ -77,6 +78,7 @@ import { TeamColorPipe } from './helpers/team-color.pipe';
     IconBadgeComponent,
     DeleteModalComponent,
     RangeComponent,
+    LeaderboardComponent,
   ],
   imports: [
     CommonModule,
@@ -139,6 +141,7 @@ import { TeamColorPipe } from './helpers/team-color.pipe';
     TopFlopComponent,
     RangeComponent,
     DeleteModalComponent,
+    LeaderboardComponent,
   ],
   providers: [NgbActiveModal],
 })
