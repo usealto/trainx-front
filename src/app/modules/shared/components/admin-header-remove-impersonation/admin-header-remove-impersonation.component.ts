4124a0745f6ab402dcdfbee7b3b6836d0ed6e52a
Guillim <guigloo@msn.com>
2023-10-02 12:23:07 +0200

    [admin] bugfix when not impersonnated as alto admin

--- src/app/modules/shared/components/admin-header-remove-impersonation/admin-header-remove-impersonation.component.ts
+++ src/app/modules/shared/components/admin-header-remove-impersonation/admin-header-remove-impersonation.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { UsersRestService } from 'src/app/modules/profile/services/users-rest.service';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/admin/admin-data.service';
+import { UserDtoApiRolesEnumApi } from '@usealto/sdk-ts-angular';
 
 @Component({
   selector: 'alto-admin-header-remove-impersonation',
   templateUrl: './admin-header-remove-impersonation.component.html',
   styleUrls: ['./admin-header-remove-impersonation.component.scss']
 })
-export class AdminHeaderRemoveImpersonationComponent {
+export class AdminHeaderRemoveImpersonationComponent implements OnInit{
 
   constructor(
-    private readonly dataService: DataService) {}
+    private readonly dataService: DataService,
+    private readonly usersRestService :UsersRestService) {}
 
   impersonatedUser =
     localStorage.getItem('impersonatedUser') !== '' && localStorage.getItem('impersonatedUser');
   impersonatedUserEmail = localStorage.getItem('impersonatedUser');
+  isAdmin = false;
+
+  ngOnInit(): void {
+    this.usersRestService.getMe().subscribe((res) => {
+      this.isAdmin = res.roles.some((r) => r === UserDtoApiRolesEnumApi.AltoAdmin)
+    })
+  }
 
   removeImpersonation() {
     localStorage.setItem('impersonatedUser', '');
