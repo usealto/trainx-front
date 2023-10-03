0fe27efea28c0e692008e4e34d3418e3f72b8f4f
Guillim <guigloo@msn.com>
2023-10-03 10:34:24 +0200

    [admin] bugfix removeImpersonation

--- src/app/modules/shared/components/admin-header-remove-impersonation/admin-header-remove-impersonation.component.ts
+++ src/app/modules/shared/components/admin-header-remove-impersonation/admin-header-remove-impersonation.component.ts
@@ -2,6 +2,8 @@ import { UsersRestService } from 'src/app/modules/profile/services/users-rest.se
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/admin/admin-data.service';
 import { UserDtoApiRolesEnumApi } from '@usealto/sdk-ts-angular';
+import { Router } from '@angular/router';
+import { ProfileStore } from 'src/app/modules/profile/profile.store';
 
 @Component({
   selector: 'alto-admin-header-remove-impersonation',
@@ -12,6 +14,8 @@ export class AdminHeaderRemoveImpersonationComponent implements OnInit{
 
   constructor(
     private readonly dataService: DataService,
+    private router : Router,
+    private readonly userstore: ProfileStore,
     private readonly usersRestService :UsersRestService) {}
 
   impersonatedUser =
@@ -27,8 +31,10 @@ export class AdminHeaderRemoveImpersonationComponent implements OnInit{
 
   removeImpersonation() {
     localStorage.setItem('impersonatedUser', '');
+    this.userstore.user.reset();
     this.impersonatedUser = false;
     this.dataService.sendData('impersonatedUserUpdated');
+    this.router.navigate(['/admin', 'home']);
   }
 
 }
