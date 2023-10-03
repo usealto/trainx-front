0d425eee342a4bf4519492f99fe114459e79c380
Thomas M <thomas@usealto.com>
2023-10-03 10:46:14 +0200

    Tests Fix

--- cypress/e2e/profile/user.cy.ts
+++ cypress/e2e/profile/user.cy.ts
@@ -7,10 +7,12 @@ describe('User Profile', () => {
 
   const lastname1 = 'testing1';
   const lastname2 = 'testing2';
-
   it('changes lastname', function () {
     cy.get('[data-cy="profileImgBadge"]').click();
     cy.get('[data-cy="profile"]').click();
+
+    cy.get('[data-cy="lastname"]').should('not.have.value', '');
+
     cy.get('[data-cy="lastname"]').clear();
 
     // changes lastname to lastname1
@@ -29,4 +31,11 @@ describe('User Profile', () => {
     cy.reload();
     cy.get('[data-cy="lastname"]').should('have.value', lastname2);
   });
+  // it('001', function () {
+  //   /* ==== Generated with Cypress Studio ==== */
+  //   cy.get('[data-cy="profileImgBadge"] > img').click();
+  //   cy.get('[data-cy="profile"] > div').click();
+  //   cy.get('[data-cy="lastname"]').should('not.be.checked');
+  //   /* ==== End Cypress Studio ==== */
+  // });
 });
