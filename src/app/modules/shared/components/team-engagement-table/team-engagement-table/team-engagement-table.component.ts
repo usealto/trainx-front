ef2666fba2d65eee167949f9418abd56cd3ca2bd
Antoine Berson <antoine.berson01@gmail.com>
2023-10-03 17:37:38 +0200

    [L/Stats] Engagement Team Table wip

--- src/app/modules/shared/components/team-engagement-table/team-engagement-table/team-engagement-table.component.ts
+++ src/app/modules/shared/components/team-engagement-table/team-engagement-table/team-engagement-table.component.ts
@@ -1,49 +1,89 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { untilDestroyed } from '@ngneat/until-destroy';
-import { TeamStatsDtoApi } from '@usealto/sdk-ts-angular';
-import { tap } from 'rxjs';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { TeamDtoApi, TeamStatsDtoApi } from '@usealto/sdk-ts-angular';
+import { combineLatest, tap } from 'rxjs';
 import { EmojiName } from 'src/app/core/utils/emoji/data';
 import { I18ns } from 'src/app/core/utils/i18n/I18n';
+import { DataForTable } from 'src/app/modules/statistics/models/statistics.model';
+import { PlaceholderDataStatus } from '../../../models/placeholder.model';
 import { ScoreDuration } from '../../../models/score.model';
 import { ScoresRestService } from '../../../services/scores-rest.service';
+import { ScoresService } from '../../../services/scores.service';
 
 @Component({
   selector: 'alto-team-engagement-table',
   templateUrl: './team-engagement-table.component.html',
   styleUrls: ['./team-engagement-table.component.scss'],
 })
-export class TeamEngagementTableComponent implements OnInit {
+export class TeamEngagementTableComponent implements OnInit, OnChanges {
   EmojiName = EmojiName;
   I18ns = I18ns;
 
   @Input() duration: ScoreDuration = ScoreDuration.Year;
 
-  teams: TeamStatsDtoApi[] = [];
-  teamsDisplay: TeamStatsDtoApi[] = [];
-  paginatedTeams: TeamStatsDtoApi[] = [];
+  teams: TeamDtoApi[] = [];
+  teamsDisplay: DataForTable[] = [];
+  paginatedTeams: DataForTable[] = [];
   teamsPage = 1;
   teamsPageSize = 5;
+  teamsDataStatus: PlaceholderDataStatus = 'good';
 
-  constructor(private readonly scoreRestService: ScoresRestService) {}
+  constructor(
+    private readonly scoreRestService: ScoresRestService,
+    private readonly scoresService: ScoresService,
+  ) {}
 
   ngOnInit(): void {
-    this.getTeamsByDuration();
+    this.getDatas();
   }
 
-  getTeamsByDuration() {
-    this.scoreRestService
-      .getTeamsStats(this.duration as ScoreDuration)
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['duration']) {
+      this.getDatas();
+    }
+  }
+
+  getDatas() {
+    combineLatest([
+      this.scoreRestService.getTeamsStats(this.duration),
+      this.scoreRestService.getTeamsStats(this.duration, true),
+    ])
       .pipe(
-        tap((t) => {
-          console.log(t);
+        tap(([teams, teamsProg]) => {
+          this.teams = teams.map((t) => t.team);
 
-          this.teams = t;
-          this.teamsDisplay = t;
-
-          this.changeTeamsPage(1);
+          this.teamsDisplay = teams.map((t) => {
+            const teamProg = teamsProg.find((tp) => tp.team.id === t.team.id);
+            return this.dataForTeamTableMapper(t, teamProg);
+          });
+          this.teamsDataStatus = teams.length === 0 ? 'noData' : 'good';
         }),
+        tap(() => this.changeTeamsPage(1)),
       )
       .subscribe();
+  }
+
+  dataForTeamTableMapper(t: TeamStatsDtoApi, tProg?: TeamStatsDtoApi) {
+    return {
+      team: t.team,
+      globalScore: t.score,
+      answeredQuestionsCount: t.totalGuessesCount,
+      answeredQuestionsProgression: this.scoresService.getProgression(
+        t.totalGuessesCount,
+        tProg?.totalGuessesCount,
+      ),
+      commentsCount: t.commentsCount,
+      commentsProgression: this.scoresService.getProgression(t.commentsCount, tProg?.commentsCount),
+      submittedQuestionsCount: t.questionsSubmittedCount,
+      submittedQuestionsProgression: this.scoresService.getProgression(
+        t.questionsSubmittedCount,
+        tProg?.questionsSubmittedCount,
+      ),
+      leastMasteredTags: t.tags
+        ?.filter((ta) => (ta.score ?? 0) < 50)
+        .sort((a, b) => (a.score || 0) - (b.score || 0))
+        .slice(0, 3)
+        .map((t) => t.tag.name),
+    } as DataForTable;
   }
 
   changeTeamsPage(page: number) {
