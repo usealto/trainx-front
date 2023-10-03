ce87751643f11227df2d123940701ed1b8db5ed4
Thomas M <thomas@usealto.com>
2023-10-03 11:25:34 +0200

    Fix Tests

--- cypress/e2e/profile/user.cy.ts
+++ cypress/e2e/profile/user.cy.ts
@@ -31,11 +31,4 @@ describe('User Profile', () => {
     cy.reload();
     cy.get('[data-cy="lastname"]').should('have.value', lastname2);
   });
-  // it('001', function () {
-  //   /* ==== Generated with Cypress Studio ==== */
-  //   cy.get('[data-cy="profileImgBadge"] > img').click();
-  //   cy.get('[data-cy="profile"] > div').click();
-  //   cy.get('[data-cy="lastname"]').should('not.be.checked');
-  //   /* ==== End Cypress Studio ==== */
-  // });
 });
