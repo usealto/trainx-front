4f0b42099669e0f105c50a090ff56ed58226b709
Guillim <guigloo@msn.com>
2023-11-08 12:21:39 +0100

    [header impersonation] part 4 setting up Automatic redirection to facilitate usage

--- src/app/layout/impersonate/impersonate.component.ts
+++ src/app/layout/impersonate/impersonate.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserDtoApi, UsersApiService } from '@usealto/sdk-ts-angular';
+import { map } from 'rxjs';
+import { UsersRestService } from 'src/app/modules/profile/services/users-rest.service';
 
 @Component({
   selector: 'alto-impersonate',
@@ -7,23 +10,53 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./impersonate.component.scss']
 })
 export class ImpersonateComponent implements OnInit{
-    user?: string;
+    userToImpersonate?: string;
+    userObj?: UserDtoApi;
+    // to show explanation if we encouter some issue
+    notFound = false; 
+    redirectAuto = false;
   
     constructor(
       private route : ActivatedRoute, 
       private router : Router,
+      private readonly usersRestService: UsersRestService,
+      private readonly userApi: UsersApiService,
       ){}
   
     ngOnInit(): void {
       this.route.params.subscribe(params => {
-        this.user = params['id'];
+        this.userToImpersonate = params['id'];
       });
+      // here we check if we should redirect automatically to avoid timeloss
+      this.redirectAuto = this.route.snapshot.queryParams['auto'];
+
+      if(this.userToImpersonate && this.redirectAuto){
+        this.impersonate();
+      } 
 
     }
 
     impersonate(){
-      localStorage.setItem('impersonatedUser', this.user || '');
-      this.router.navigate(['/']);
+      localStorage.setItem('impersonatedUser', this.userToImpersonate || '');
+      
+      // here we try to fetch the user, and if cannot be found we cancel the impersonation
+      // we must skip the store here
+      try {
+        this.userApi.getMe().pipe(
+          map((u) => u.data || ({} as UserDtoApi)),
+        ).subscribe((res) => {
+          this.userObj = res
+          if(!res){
+            this.notFound = true;
+            localStorage.setItem('impersonatedUser', '');
+          }else if(this.redirectAuto){
+            window.location.href = '/'; // this is the only way to refresh the page
+          }
+        }) 
+      } catch (e) {
+        this.notFound = true;
+        localStorage.setItem('impersonatedUser', '');
+      }
     }
 
 }
