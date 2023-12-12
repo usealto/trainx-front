89227d57432a05d45559d9b4c8431a12b5ab02f3
oliviermlm <olivier@usealto.com>
2023-12-12 11:18:53 +0100

    Revert "Revert "Merge pull request #326 from usealto/dns""
    This reverts commit 791c776456bc05f4adb1dab6e561f11e2d1d3518.

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://trainx-api-develop.usealto.com',
+  apiURL: 'https://trainx-api-develop.getcockpit.io',
   auth0Domain: 'dev-bmttww5s.eu.auth0.com',
   auth0ClientId: 'ThcIBQZrRso5QaZq67kCU5eFYTfZwTSK',
   audience: 'https://api.usealto.com',
