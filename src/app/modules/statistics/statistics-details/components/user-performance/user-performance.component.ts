c2fcd99a539d4fcd7afbc4db6fb330007ba1c05d
Hugo Poisot <poisot.hugo@gmail.com>
2024-01-15 17:43:07 +0100

    Fix "empty" placeholders

--- src/app/modules/statistics/statistics-details/components/user-performance/user-performance.component.ts
+++ src/app/modules/statistics/statistics-details/components/user-performance/user-performance.component.ts
@@ -25,6 +25,7 @@ import { ScoresService } from 'src/app/modules/shared/services/scores.service';
 import { IAppData } from '../../../../../core/resolvers';
 import { ToastService } from '../../../../../core/toast/toast.service';
 import { StatisticsService } from '../../../services/statistics.service';
+import { AltoRoutes } from '../../../../shared/constants/routes';
 
 @Component({
   selector: 'alto-user-performance',
@@ -34,6 +35,7 @@ import { StatisticsService } from '../../../services/statistics.service';
 export class UserPerformanceComponent implements OnInit {
   I18ns = I18ns;
   EmojiName = EmojiName;
+  AltoRoutes = AltoRoutes;
 
   user!: User;
   userTeam!: Team;
@@ -96,7 +98,6 @@ export class UserPerformanceComponent implements OnInit {
   }
 
   getSpiderChartScores(): void {
-    this.spiderChartSectionStatus = this.tags.length ? 'good' : 'empty';
     this.spiderChartStatus = 'loading';
     combineLatest([
       this.scoresRestService.getTagsStats(
@@ -111,7 +112,8 @@ export class UserPerformanceComponent implements OnInit {
         teamStats.sort((a, b) => a.tag.name.localeCompare(b.tag.name)),
         userStats.sort((a, b) => a.label.localeCompare(b.label)),
       );
-      this.spiderChartStatus = 'good';
+      this.spiderChartStatus = userStats.length ? 'good' : 'noData';
+      this.spiderChartSectionStatus = this.tags.length < 3 ? 'empty' : userStats.length ? 'good' : 'noData';
     });
   }
 
