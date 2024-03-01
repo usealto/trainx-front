bfb402191a56223e29d22f566bfaba02a2dd9746
Guillim <guillim@users.noreply.github.com>
2024-03-01 12:34:29 +0100

    removing localhost sentry events

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,4 +11,5 @@ export const environment = {
   sentryTracesSampleRate: 1.0,
   sentryReplaysSessionSampleRate: 0,
   sentryReplaysOnErrorSampleRate: 1.0,
+  sentryEnvironment: 'development',
 };
--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,27 @@ import * as Sentry from '@sentry/angular-ivy';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-Sentry.init({
-  dsn: environment.sentryDsn,
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ['localhost', /^https:\/\/getcockpit\.io/],
-    }),
-    new Sentry.Replay({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: environment.sentryTracesSampleRate, //  Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: environment.sentryReplaysSessionSampleRate, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: environment.sentryReplaysOnErrorSampleRate, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+if (environment.production) {
+  Sentry.init({
+    dsn: environment.sentryDsn,
+    environment: environment.sentryEnvironment === 'production' ? 'production' : 'development',
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ['localhost', /^https:\/\/getcockpit\.io/],
+      }),
+      new Sentry.Replay({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: environment.sentryTracesSampleRate, //  Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: environment.sentryReplaysSessionSampleRate, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: environment.sentryReplaysOnErrorSampleRate, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
