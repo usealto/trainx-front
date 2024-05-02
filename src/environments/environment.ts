612cdb62aae9807b85221e356fdb707bc1c6b78a
Jérémy Moyson <moyson.jeremy@gmail.com>
2024-05-02 11:44:54 +0200

    Replace dev API url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://trainx-api-develop.getcockpit.io/',
+  apiURL: 'http://localhost:3000',
   auth0Domain: 'dev-bmttww5s.eu.auth0.com',
   auth0ClientId: 'ThcIBQZrRso5QaZq67kCU5eFYTfZwTSK',
   audience: 'https://api.usealto.com',
