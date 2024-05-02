c336ea42b6aad85330ae6c0346f5bd30e091fb98
Maxime Frechard <maxime.frechard@epitech.eu>
2024-05-02 15:08:20 +0200

    fix test and change style disbaled prog

--- src/app/modules/parcours/services/parcours-rest.service.spec.ts
+++ src/app/modules/parcours/services/parcours-rest.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ParcoursRestService } from './parcours-rest.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ParcoursRestService', () => {
   let service: ParcoursRestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ParcoursRestService],
+    });
     service = TestBed.inject(ParcoursRestService);
   });
 
