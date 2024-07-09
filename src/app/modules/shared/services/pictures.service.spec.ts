3e8c4187509492249cd2595f80450568ca43970b
Maxime Frechard <maxime.frechard@epitech.eu>
2024-07-09 14:40:31 +0200

    fix tests

--- src/app/modules/shared/services/pictures.service.spec.ts
+++ src/app/modules/shared/services/pictures.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PicturesService } from './pictures.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PicturesService', () => {
   let service: PicturesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+     TestBed.configureTestingModule({
+       imports: [HttpClientTestingModule],
+       providers: [PicturesService],
+     });
     service = TestBed.inject(PicturesService);
   });
 
